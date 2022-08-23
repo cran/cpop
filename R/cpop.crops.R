6118353b8eb3a2f94fa81d192da9708eeef69877
Daniel Grose <dan.grose@lancaster.ac.uk>
2022-08-23 12:10:02 +0000

    version 1.0.3

--- R/cpop.crops.R
+++ R/cpop.crops.R
@@ -10,9 +10,9 @@ cpop.crops.class<-function(crops.result,y,x)
 #'
 #' @name cpop.crops
 #'
-#' @description Runs the Changepoints for a Range of Penalties (CROPS) algorithm of Haynes et al. (2014) to find all of the optimal segmentations for multiple penalty values over a continuous range.
-#' For details of the \code{crops} function see the \pkg{crops package}. Methods from the \pkg{crops} package that are applicable to crops are
-#' \code{\link[crops]{segmentations}} and \code{\link[crops]{subset}}.    
+#' @description Runs the Changepoints for a Range of Penalties (CROPS) algorithm of Haynes et al. (2017) to find all of the optimal segmentations for multiple penalty values over a continuous range.
+#' For details of the CROPS method see the \pkg{\link[crops]{crops}} package. To obtain details for each segmentation determined by \code{\link[cpop]{cpop.crops}} use the 
+#' \code{\link[crops]{segmentations}} method (see example below).    
 #'
 #' @param y A vector of length n containing the data.
 #' @param x A vector of length n containing the locations of y. Default value is NULL, in which case the locations \code{x = 1:length(y)-1} are assumed.
@@ -21,8 +21,8 @@ cpop.crops.class<-function(crops.result,y,x)
 #' @param beta_max Maximum value of the penalty range to be searched. Default is \code{2.5*log(length(y))} 
 #' @param sd Estimate of residual standard deviation. Can be a single numerical value or a vector of values for the case of varying standard deviation. Default value is 1.
 #' @param minseglen The minimum allowable segment length, i.e. distance between successive changepoints. Default is 0.
-#' @param prune.approx Only relevant if a minimum segment length is set. If True, cpop will use an approximate pruning algorithm that will speed up computation but may
-#' occasionally lead to a sub-optimal solution in terms of the estimate change point locations. If the minimum segment length is 0, then an exact pruning algorithm is possible and is used.
+#' @param prune.approx Only relevant if a minimum segment length is set. If TRUE, cpop will use an approximate pruning algorithm that will speed up computation but may
+#' occasionally lead to a sub-optimal solution in terms of the estimate change point locations. If the minimum segment length is 0, then an exact pruning algorithm is possible and is used. 
 #'
 #' @return An instance of an S4 class of type cpop.crops.class which extends the crops.class in \pkg{crops}.
 #'
@@ -38,21 +38,21 @@ cpop.crops.class<-function(crops.result,y,x)
 #' set.seed(0)
 #' x <- seq(0,1,0.01)
 #' n <- length(x)
-#' sigma <- rep(0.1,n)
+#' sd <- rep(0.1,n)
 #' mu <- c(2*x[1:floor(n/2)],2 - 2*x[(floor(n/2)+1):n])
-#' y <- rnorm(n,mu,sigma)
+#' y <- rnorm(n,mu,sd)
 #' # calculate the changepoint locations and cost over a range of penalty values
-#' res <- cpop.crops(y,x,sd=sigma,beta_min=0.4*log(length(y)),beta_max=2.5*log(length(y)))
+#' res <- cpop.crops(y,x,sd=sd,beta_min=0.4*log(length(y)),beta_max=2.5*log(length(y)))
 #' summary(res)
 #'
 #' # plot the results
 #' plot(res) 
 #'
 #' # show the segmentations
-#  segmentations(res)
+#'  segmentations(res)
 #' }
 #'
-#' @references \insertRef{crops-article}{crops}
+#' @references \insertRef{crops-article}{cpop}
 #' @references \insertRef{crops-package}{cpop}
 #' @export
 cpop.crops<-function(y,x = 1:length(y),grid = x, beta_min = 1.5 * log(length(y)),
@@ -102,7 +102,7 @@ cpop.crops<-function(y,x = 1:length(y),grid = x, beta_min = 1.5 * log(length(y))
 #' n <- 500
 #' x <- 1:n
 #' m <- 10
-#' mu <- simulate(x,changepoints=(n/(m+1))*0:m,change.slope=c(0.1,0.2*(-1)^(1:m)),sigma=0)
+#' mu <- simchangeslope(x,changepoints=(n/(m+1))*0:m,change.slope=c(0.1,0.2*(-1)^(1:m)),sd=0)
 #' epsilon <- rnorm(n+2)
 #' y <- mu+(epsilon[1:n]+epsilon[2:(n+1)]+epsilon[3:(n+2)])/sqrt(3)
 #' res.crops <- cpop.crops(y,x,beta_min=0.5*log(length(y)),beta_max=40*log(length(y)))
@@ -115,7 +115,7 @@ cpop.crops<-function(y,x = 1:length(y),grid = x, beta_min = 1.5 * log(length(y))
 #'
 #' @seealso \code{\link{cpop.crops}},\code{\link[crops]{crops}}
 #'
-#' @references \insertRef{crops-article}{crops}
+#' @references \insertRef{crops-article}{cpop}
 #' @references \insertRef{crops-package}{cpop}
 #' @export
 setGeneric("cpop.crops.models",function(object) {standardGeneric("cpop.crops.models")})
